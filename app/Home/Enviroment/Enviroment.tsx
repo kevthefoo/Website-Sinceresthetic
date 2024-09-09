83e30187e823632cff9c9b943ec4e8d4464c1fe8
KevTheFoo <kevthefoo@gmail.com>
2024-09-09 22:48:06 +1000

    Refactor Home component: Add Enviroment component

---
+++ app/Home/Enviroment/Enviroment.tsx
@@ -0,0 +1,9 @@
+const Enviroment: React.FC = () => {
+    return (
+        <section>
+            <h1>極致舒適的醫療體驗</h1>
+        </section>
+    );
+};
+
+export default Enviroment;
--- app/Home/Home.tsx
+++ app/Home/Home.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Hero from "./Hero/Hero";
 import Introduction from "./Introduction/Introduction";
 import Services from "./Services/Services";
-
+import Enviroment from "./Enviroment/Enviroment";
 const Home: React.FC = () => {
     return (
         <>
             <Hero />
             <Introduction />
             <Services />
+            <Enviroment />
         </>
     );
 };
