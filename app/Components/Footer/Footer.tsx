6ff069a7bc1303dfdfbdf43d7919b0bb11d2dd2a
KevTheFoo <kevthefoo@gmail.com>
2024-09-10 01:12:23 +1000

    Refactor Footer and Header components: Update styling and add copyright notice

--- app/Components/Footer/Footer.tsx
+++ app/Components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+const currentYear = new Date().getFullYear();
 
 const Footer: React.FC = () => {
     return (
-        <footer>This is footer</footer>
+        <footer className="border-t-2 h-20 flex flex-col justify-end items-center px-32 py-2">
+            <div>Copyright© {currentYear} 澄臻美學牙醫診所</div>
+        </footer>
     );
-}
+};
 
 export default Footer;
