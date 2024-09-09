d9bff389917f5994cc1627bb3889faada68534b5
KevTheFoo <kevthefoo@gmail.com>
2024-09-09 22:38:25 +1000

    Refactor Card component: Add Card, CardHeader, CardTitle, CardDescription, CardContent, and CardFooter components

--- app/Home/Services/Services.tsx
+++ app/Home/Services/Services.tsx
@@ -1,29 +1,190 @@
 import React from "react";
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel";
+
+import service_1 from "@/public/images/services/service_1.png";
+import service_2 from "@/public/images/services/service_2.png";
+import service_3 from "@/public/images/services/service_3.png";
+import service_4 from "@/public/images/services/service_4.png";
+import service_5 from "@/public/images/services/service_5.png";
+import service_6 from "@/public/images/services/service_6.png";
+import service_7 from "@/public/images/services/service_7.png";
+import service_8 from "@/public/images/services/service_8.png";
+import service_9 from "@/public/images/services/service_9.png";
 
 const Services: React.FC = () => {
     return (
         <section
-            className="flex flex-col justify-start items-center"
+            className="flex flex-col justify-start items-center h-screen"
             id="services"
         >
             <h1>全方位醫療項目</h1>
-            {/* <Carousel
-                opts={{
-                    align: "start",
-                    loop: true,
-                }}
-            >
-                <CarouselContent>
-                    <CarouselItem>...</CarouselItem>
-                    <CarouselItem>...</CarouselItem>
-                    <CarouselItem>...</CarouselItem>
+            <Carousel className="w-full h-full border-2 border-red-500 flex flex-col justify-center">
+                <CarouselContent className="border-2 border-black">
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_1}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>牙齒矯正</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_2}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>牙齒修護</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_3}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>牙齒矯正</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_4}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>全植牙</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_5}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>全瓷冠</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_6}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>牙齒美白</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_7}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>兒童牙科</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_8}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>家庭牙醫</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4 border-2 h-full border-red-500">
+                        <div className="border-2 border-yellow-500">
+                            <Card>
+                                <CardContent className="p-0 rounded-[50px] overflow-hidden flex flex-col aspect-square items-center justify-start border-2 border-green-600">
+                                    <Image
+                                        src={service_9}
+                                        alt="service_1"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                        placeholder="blur"
+                                        className="border-2 border-green-600 mb-4"
+                                    />
+                                    <p>3D齒雕</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
                 </CarouselContent>
-            </Carousel> */}
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </section>
     );
 };
