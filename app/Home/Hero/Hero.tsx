df1ba2fabcfcaac5fc806001bc61fdf4f29a7f21
KevTheFoo <kevthefoo@gmail.com>
2024-09-09 14:13:12 +1000

    Refactor Header component and add background to Hero component

--- app/Home/Hero/Hero.tsx
+++ app/Home/Hero/Hero.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./hero.css";
 
 const Hero: React.FC = () => {
     return (
         <section className="px-0">
-            <div className="rounded-tl-[500px] h-screen w-full border-2  border-blue-400"></div>
+            <div className="hero__background__container rounded-tl-[500px] h-screen w-full border-2 border-blue-400"></div>
         </section>
     );
 };
