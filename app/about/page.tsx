4ff72d035fff35efcdf705643eca2a37f153ad93
KevTheFoo <kevthefoo@gmail.com>
2024-09-10 12:04:42 +1000

    Refactor About page: Add banner image to the section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from "react";
 import type { Metadata } from "next";
+import Image from "next/image";
+import banner from "@/public/images/logo/banner_2.png";
 
 export const metadata: Metadata = {
     title: "關於澄臻",
@@ -8,8 +10,20 @@ export const metadata: Metadata = {
 
 const About: React.FC = () => {
     return (
-        <section>This is about</section>
+        <section>
+            <div className="banner__container absolute w-full overflow-hidden left-0 h-32">
+                <Image
+                    src={banner}
+                    alt="banner"
+                    fill={true}
+                    objectFit="cover"
+                    objectPosition="center"
+                    priority={true}
+                    placeholder="blur"
+                />
+            </div>
+        </section>
     );
-}
+};
 
 export default About;
