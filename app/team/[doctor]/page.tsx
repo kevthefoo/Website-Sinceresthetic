039fc01450a235ad363f4c4641e8943b46f5b407
KevTheFoo <kevthefoo@gmail.com>
2024-09-10 22:12:42 +1000

    Refactor team page: Add border and shadow to doctor image

--- app/team/[doctor]/page.tsx
+++ app/team/[doctor]/page.tsx
@@ -23,7 +23,7 @@ const Dentist: React.FC<DentistProps> = ({ params }) => {
                 {doctorName} {doctorTitle}
             </h1>
             <div className="flex justify-evenly items-end">
-                <div className="self-stretch">
+                <div className="self-stretch border-4 border-slate-400 rounded-xl overflow-hidden shadow-lg">
                     <Image
                         src={imagePath}
                         alt={doctor}
