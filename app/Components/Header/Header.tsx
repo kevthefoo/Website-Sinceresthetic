f7059c1c08ed9820f689a15055228974ba35bd9c
KevTheFoo <kevthefoo@gmail.com>
2024-09-11 08:19:51 +1000

    Add prettier-plugin-tailwindcss to project dependencies

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FaInstagramSquare, FaFacebookSquare, FaLine } from "react-icons/fa";
 
 import logo from "@/public/images/logo/logo_new.png";
@@ -5,113 +7,192 @@ import Image from "next/image";
 
 import Link from "next/link";
 import React from "react";
+import { useState, useEffect } from "react";
+
 import {
-    navigationMenuTriggerStyle,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenu,
+  navigationMenuTriggerStyle,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenu,
 } from "@/components/ui/navigation-menu";
 
 const Header: React.FC = () => {
-    return (
-        <header className="fixed w-full flex border-b-2 items-center justify-between px-32 py-2 h-20  bg-white z-[100]">
-            {/* Logo */}
-            <Link href="/" className="flex items-center justify-center gap-2">
-                <Image src={logo} alt="logo" height={50} />
-                <div className="select-none">
-                    <h5 className="text-xl">澄臻美學牙醫診所</h5>
-                    <h5 className="text-[13px]">
-                        Sinceresthetic Dental Clinic
-                    </h5>
-                </div>
-            </Link>
+  const [active, setActive] = useState(false);
 
-            {/* Navigation Menu */}
-            <NavigationMenu className="">
-                <NavigationMenuItem className="list-none hover:text-gray-500 hover:border-b-2 hover:border-gray-500">
-                    <Link href="/" legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={`${navigationMenuTriggerStyle()} text-base`}
-                        >
-                            首頁
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
+  const handleActive = () => {
+    setActive(!active);
+  };
 
-                <NavigationMenuItem className="list-none hover:text-gray-500 hover:border-b-2 hover:border-gray-500">
-                    <Link href="/about" legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={`${navigationMenuTriggerStyle()} text-base`}
-                        >
-                            關於澄臻
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
+  useEffect(() => {
+    if (active) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [active]);
 
-                <NavigationMenuItem className="list-none hover:text-gray-500 hover:border-b-2 hover:border-gray-500">
-                    <Link href="/services" legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={`${navigationMenuTriggerStyle()} text-base`}
-                        >
-                            診療項目
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
+  return (
+    <header className="max-rg_desktop:px-8 fixed z-[100] flex h-20 w-full items-center justify-between border-b-2 bg-white px-32 py-2">
+      {/* Logo */}
+      <Link href="/" className="flex items-center justify-center gap-2">
+        <Image src={logo} alt="logo" height={50} />
+        <div className="select-none">
+          <h5 className="text-xl">澄臻美學牙醫診所</h5>
+          <h5 className="text-[13px]">Sinceresthetic Dental Clinic</h5>
+        </div>
+      </Link>
 
-                <NavigationMenuItem className="list-none hover:text-gray-500 hover:border-b-2 hover:border-gray-500">
-                    <Link href="/team" legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={`${navigationMenuTriggerStyle()} text-base`}
-                        >
-                            醫療團隊
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem className="list-none hover:text-gray-500 hover:border-b-2 hover:border-gray-500">
-                    <Link
-                        href="https://page.line.me/920oaogn?openQrModal=true"
-                        legacyBehavior
-                        passHref
-                    >
-                        <NavigationMenuLink
-                            target="_blank"
-                            className={`${navigationMenuTriggerStyle()} text-base`}
-                        >
-                            立即預約
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-            </NavigationMenu>
+      {/* Navigation Menu */}
+      <NavigationMenu className="max-lg_tablet:hidden">
+        <NavigationMenuItem className="list-none hover:border-b-2 hover:border-gray-500 hover:text-gray-500">
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={`${navigationMenuTriggerStyle()} text-base`}
+            >
+              首頁
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
 
-            {/* Social Media Icons */}
-            <div className="flex items-center justify-center gap-2">
-                <Link
-                    href="https://instagram.com/sinceresthetic"
-                    target="_blank"
-                >
-                    <i className="text-2xl">
-                        <FaInstagramSquare />
-                    </i>
-                </Link>
-                <Link
-                    href="https://www.facebook.com/sinceresthetic"
-                    target="_blank"
-                >
-                    <i className="text-2xl">
-                        <FaFacebookSquare />
-                    </i>
-                </Link>
-                <Link
-                    href="https://page.line.me/920oaogn?openQrModal=true"
-                    target="_blank"
-                >
-                    <i className="text-2xl">
-                        <FaLine />
-                    </i>
-                </Link>
-            </div>
-        </header>
-    );
+        <NavigationMenuItem className="list-none hover:border-b-2 hover:border-gray-500 hover:text-gray-500">
+          <Link href="/about" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={`${navigationMenuTriggerStyle()} text-base`}
+            >
+              關於澄臻
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem className="list-none hover:border-b-2 hover:border-gray-500 hover:text-gray-500">
+          <Link href="/services" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={`${navigationMenuTriggerStyle()} text-base`}
+            >
+              診療項目
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem className="list-none hover:border-b-2 hover:border-gray-500 hover:text-gray-500">
+          <Link href="/team" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={`${navigationMenuTriggerStyle()} text-base`}
+            >
+              醫療團隊
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="list-none hover:border-b-2 hover:border-gray-500 hover:text-gray-500">
+          <Link
+            href="https://page.line.me/920oaogn?openQrModal=true"
+            legacyBehavior
+            passHref
+          >
+            <NavigationMenuLink
+              target="_blank"
+              className={`${navigationMenuTriggerStyle()} text-base`}
+            >
+              立即預約
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenu>
+
+      {/* Social Media Icons */}
+      <div className="max-lg_tablet:hidden flex items-center justify-center gap-2">
+        <Link href="https://instagram.com/sinceresthetic" target="_blank">
+          <i className="text-2xl">
+            <FaInstagramSquare />
+          </i>
+        </Link>
+        <Link href="https://www.facebook.com/sinceresthetic" target="_blank">
+          <i className="text-2xl">
+            <FaFacebookSquare />
+          </i>
+        </Link>
+        <Link
+          href="https://page.line.me/920oaogn?openQrModal=true"
+          target="_blank"
+        >
+          <i className="text-2xl">
+            <FaLine />
+          </i>
+        </Link>
+      </div>
+
+      {/* Mobile Menu Toggle Button*/}
+      <div
+        className="max-lg_tablet:flex absolute right-8 top-[50%] z-50 hidden h-7 w-7 translate-y-[-50%] cursor-pointer flex-col justify-between"
+        onClick={() => {
+          handleActive();
+        }}
+      >
+        <div
+          className={
+            active
+              ? "absolute left-0 top-1/2 h-1 w-full rotate-45 bg-black transition ease-in-out"
+              : "h-1 w-full bg-black transition ease-in-out"
+          }
+        ></div>
+        <div
+          className={
+            active ? "" : "h-1 w-full bg-black transition delay-150 ease-in-out"
+          }
+        ></div>
+        <div
+          className={
+            active
+              ? "absolute left-0 top-1/2 h-1 w-full rotate-[-45deg] bg-black transition ease-in-out"
+              : "h-1 w-full bg-black transition ease-in-out"
+          }
+        ></div>
+      </div>
+
+      {/* Mobile Menu*/}
+      <div
+        className={
+          active
+            ? "fixed left-0 top-0 z-30 flex h-screen w-full flex-col items-center justify-start bg-white pt-[300px] transition duration-700 ease-linear"
+            : "fixed left-full top-0 z-30 flex h-screen w-full items-center justify-center bg-white transition duration-700 ease-linear"
+        }
+      >
+        <nav className={active ? "" : ""}>
+          <ul className="flex flex-col justify-center gap-8 text-center">
+            <li className="relative before:absolute before:right-[95px] before:top-[10px] before:h-[1px] before:w-16 before:bg-black before:content-[''] after:absolute after:left-[95px] after:top-[10px] after:h-[1px] after:w-16 after:bg-black after:content-['']">
+              <Link href="/" onClick={() => setActive(false)}>
+                首頁
+              </Link>
+            </li>
+            <li className="relative before:absolute before:right-[95px] before:top-[10px] before:h-[1px] before:w-16 before:bg-black before:content-[''] after:absolute after:left-[95px] after:top-[10px] after:h-[1px] after:w-16 after:bg-black after:content-['']">
+              <Link href="/about" onClick={() => setActive(false)}>
+                關於澄臻
+              </Link>
+            </li>
+            <li className="relative before:absolute before:right-[95px] before:top-[10px] before:h-[1px] before:w-16 before:bg-black before:content-[''] after:absolute after:left-[95px] after:top-[10px] after:h-[1px] after:w-16 after:bg-black after:content-['']">
+              <Link href="/services" onClick={() => setActive(false)}>
+                診療項目
+              </Link>
+            </li>
+            <li className="relative before:absolute before:right-[95px] before:top-[10px] before:h-[1px] before:w-16 before:bg-black before:content-[''] after:absolute after:left-[95px] after:top-[10px] after:h-[1px] after:w-16 after:bg-black after:content-['']">
+              <Link href="/team" onClick={() => setActive(false)}>
+                醫療團隊
+              </Link>
+            </li>
+            <li className="relative before:absolute before:right-[95px] before:top-[10px] before:h-[1px] before:w-16 before:bg-black before:content-[''] after:absolute after:left-[95px] after:top-[10px] after:h-[1px] after:w-16 after:bg-black after:content-['']">
+              <Link
+                href="https://page.line.me/920oaogn?openQrModal=true"
+                target="_blank"
+                onClick={() => setActive(false)}
+              >
+                立即預約
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,117 +1,97 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-    	extend: {
-    		colors: {
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			}
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		animation: {
-    			show: 'cycleImages 10s ease-in-out infinite',
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
-    		},
-    		keyframes: {
-    			cycleImages: {
-    				'0%': {
-    					bg: 'bg-[url("/public/images/clinic/sincere_0.png")]'
-    				},
-    				'20%': {
-    					bg: 'bg-[url("/public/images/clinic/sincere_1.png")]'
-    				},
-    				'40%': {
-    					bg: 'bg-[url("/public/images/clinic/sincere_2.png")]'
-    				},
-    				'60%': {
-    					bg: 'bg-[url("/public/images/clinic/sincere_3.png")]'
-    				},
-    				'80%': {
-    					bg: 'bg-[url("/public/images/clinic/sincere_4.png")]'
-    				},
-    				'100%': {
-    					bg: 'bg-[url("/public/images/clinic/sincere_0.png")]'
-    				}
-    			},
-    			boo: {
-    				'0%': {
-    					bg: 'bg-red-500'
-    				},
-    				'50%': {
-    					bg: 'bg-yellow-500'
-    				},
-    				'100%': {
-    					bg: 'bg-green-500'
-    				}
-    			},
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			}
-    		}
-    	}
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      animation: {
+        show: "cycleImages 10s ease-in-out infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
     },
-    plugins: [require("tailwindcss-animate")],
+    screens: {
+      sm_mobile: "320px",
+      rg_mobile: "480px",
+      lg_mobile: "576px",
+      sm_tablet: "640px",
+      rg_tablet: "768px",
+      lg_tablet: "992px",
+      sm_desktop: "1024px",
+      rg_desktop: "1280px",
+      lg_desktop: "1536px",
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
