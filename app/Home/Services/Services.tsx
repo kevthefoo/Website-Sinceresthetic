91b37395526e4a3fe9b78ade1c02eb371dfc7db7
KevTheFoo <kevthefoo@gmail.com>
2024-09-12 00:47:44 +1000

    Refactor Services component: Adjust section styles for better alignment

--- app/Home/Services/Services.tsx
+++ app/Home/Services/Services.tsx
@@ -3,7 +3,7 @@ import ServicesCarousel from "./ServicesCarousel";
 const Services: React.FC = () => {
   return (
     <section
-      className="border-4 border-red-500 relative flex h-screen flex-col items-center justify-center bg-gradient-to-t from-white to-gray-100"
+      className="relative flex flex-col items-center justify-start bg-gradient-to-t from-white to-gray-100"
       id="services"
     >
       <h1>全方位醫療項目</h1>
