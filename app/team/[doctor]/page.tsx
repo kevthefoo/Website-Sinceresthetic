4523d9355f48e47199a439a08965420f338a4f52
KevTheFoo <kevthefoo@gmail.com>
2024-09-12 22:41:37 +1000

    Refactor Dentist page: Adjust layout and styles

--- app/team/[doctor]/page.tsx
+++ app/team/[doctor]/page.tsx
@@ -4,107 +4,104 @@ import DoctorData from "../DoctorData";
 import "./doctor.css";
 
 interface DentistProps {
-    params: {
-        doctor: string;
-    };
+  params: {
+    doctor: string;
+  };
 }
 
 const Dentist: React.FC<DentistProps> = ({ params }) => {
-    const { doctor } = params;
-    const doctorName = DoctorData[doctor].chineseName;
-    const doctorTitle = DoctorData[doctor].title;
-    const doctorEducation = DoctorData[doctor].education;
-    const doctorExperience = DoctorData[doctor].experience;
+  const { doctor } = params;
+  const doctorName = DoctorData[doctor].chineseName;
+  const doctorTitle = DoctorData[doctor].title;
+  const doctorEducation = DoctorData[doctor].education;
+  const doctorExperience = DoctorData[doctor].experience;
 
-    const imagePath = `/images/doctor/${doctor}.png`;
-    return (
-        <section className="flex flex-col justify-center bg-gradient-to-b from-white to-gray-100">
-            <h1>
-                {doctorName} {doctorTitle}
-            </h1>
-            <div className="flex justify-evenly items-end">
-                <div className="self-stretch border-4 border-slate-400 rounded-xl overflow-hidden shadow-lg">
-                    <Image
-                        src={imagePath}
-                        alt={doctor}
-                        width={300}
-                        height={300}
-                        priority={true}
-                    />
-                </div>
+  const imagePath = `/images/doctor/${doctor}.png`;
+  return (
+    <section className="flex flex-col justify-center bg-gradient-to-b from-white to-gray-100 max-lg_tablet:pb-12 max-lg_tablet:pt-24">
+      <h1>
+        {doctorName} {doctorTitle}
+      </h1>
+      <div className="flex items-end justify-evenly max-lg_tablet:flex-col max-lg_tablet:items-center max-lg_tablet:justify-center max-lg_tablet:gap-12">
+        <div className="relative aspect-[3/4] h-[330px] overflow-hidden rounded-xl border-4 border-slate-400 shadow-lg max-lg_tablet:aspect-square max-lg_tablet:h-auto max-lg_tablet:w-[200px] max-lg_tablet:rounded-full">
+          <Image
+            src={imagePath}
+            alt={doctor}
+            fill={true}
+            priority={true}
+            style={{
+              objectFit: "cover",
+              objectPosition: "top",
+            }}
+          />
+        </div>
 
-                <div className="">
-                    <h2 className="mb-4">
-                        {doctorName} {doctorTitle}
-                    </h2>
+        <div className="">
+          <div className="mb-4 flex">
+            <p className="mr-2">專科:</p>
 
-                    <div className="flex mb-4">
-                        <h2 className="mr-2">專科:</h2>
+            <ol className="flex items-center justify-center text-xl">
+              {DoctorData[doctor].speciality.map((speciality, index) => (
+                <li key={index} className="mr-2">
+                  <p>{speciality}/</p>
+                </li>
+              ))}
+            </ol>
+          </div>
 
-                        <ol className="flex items-center justify-center text-xl">
-                            {DoctorData[doctor].speciality.map(
-                                (speciality, index) => (
-                                    <li key={index} className="mr-2">
-                                        {speciality}/
-                                    </li>
-                                )
-                            )}
-                        </ol>
-                    </div>
+          <p className="mb-4">學歷: {doctorEducation}</p>
 
-                    <h2 className="mb-4">學歷: {doctorEducation}</h2>
+          <p className="mb-4">經歷: {doctorExperience}</p>
 
-                    <h2 className="mb-4">經歷: {doctorExperience}</h2>
-
-                    <div className="">
-                        <h2>看診時間</h2>
-                        <table className="styled-table">
-                            <thead>
-                                <tr>
-                                    <th>時間</th>
-                                    <th>一</th>
-                                    <th>二</th>
-                                    <th>三</th>
-                                    <th>四</th>
-                                    <th>五</th>
-                                    <th>六</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>09:00-12:00</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>15:00-18:00</td>
-                                    <td></td>
-                                    <td>✓</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>✓</td>
-                                </tr>
-                                <tr>
-                                    <td>18:00-21:00</td>
-                                    <td></td>
-                                    <td>✓</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>✓</td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          <div className="">
+            <p>看診時間</p>
+            <table className="styled-table">
+              <thead>
+                <tr>
+                  <th>時間</th>
+                  <th>一</th>
+                  <th>二</th>
+                  <th>三</th>
+                  <th>四</th>
+                  <th>五</th>
+                  <th>六</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>09:00-12:00</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>15:00-18:00</td>
+                  <td></td>
+                  <td>✓</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>✓</td>
+                </tr>
+                <tr>
+                  <td>18:00-21:00</td>
+                  <td></td>
+                  <td>✓</td>
+                  <td></td>
+                  <td></td>
+                  <td>✓</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dentist;
