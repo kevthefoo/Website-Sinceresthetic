e966884da1eb9948066180500cbb1683d29a2f70
KevTheFoo <kevthefoo@gmail.com>
2024-09-16 22:02:25 +1000

    Refactor logo file format to webp in Header component

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -2,7 +2,7 @@
 
 import { FaInstagramSquare, FaFacebookSquare, FaLine } from "react-icons/fa";
 
-import logo from "@/public/images/logo/logo.png";
+import logo from "@/public/images/logo/logo.webp";
 import Image from "next/image";
 
 import Link from "next/link";
